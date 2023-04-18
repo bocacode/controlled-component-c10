80cadf5b29dd5b8235e5cb3cb75776acf0b7763a
Todd H. Albert <todd@bocacode.com>
2023-04-18 12:00:41 -0400

    testing controlled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [name, setName] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <form>
+          <label htmlFor='name'>
+            Name:
+            <input type='text' name='name'
+              onChange={e => setName(e.target.value)} value={name} />
+          </label>
+        </form>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Test.jsx
@@ -0,0 +1,12 @@
+export default function Test() {
+  return (
+    <h1>Test</h1>
+  )
+}
+
+// 1. Components
+// 2. useState()
+// 3. props
+// 4. useEffect(() => {}, [])
+// ...
+// 10. controlled components
